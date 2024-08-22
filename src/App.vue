63ceeaef0d67695a05f56707c1d3138e16c62850
Nicole <pinhsiuwang@gmail.com>
2024-08-22 18:04:39 +0800

    0822 final

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
       <navbar></navbar>
+
     </div>
     <RouterView></RouterView>
 </template>
   
 <script setup>
   import navbar from './views/navbar.vue';
+
 
   import 'bootstrap/dist/css/bootstrap.min.css';
   import 'bootstrap/dist/js/bootstrap.bundle.min.js';
--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -1,9 +1,44 @@
 <template>
-    <img :src="`${photo}`" alt="Profile Photo" />
+    <!-- <img :src="`${photo}`" alt="Profile Photo" />
 
     <Vueform ref="form$">
         <ButtonElement name="editProfile" button-label="編輯個人資料" :full="true" size="lg" @click="editProfile"/>
-    </Vueform>
+    </Vueform> -->
+  <div class="profile-page">
+    <!-- 侧边栏导航 -->
+    <aside class="sidebar">
+      <ul>
+        <li>Events</li>
+        <li>News</li>
+        <li>Chats</li>
+        <li class="active">Profile</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+
+    <!-- 主要内容部分 -->
+    <section class="profile-content">
+      <!-- 用户信息卡片 -->
+      <div class="user-card">
+        <img :src="`${photo}`" alt="User Photo" class="user-photo" />
+        <h3>{{ nickname }}</h3>
+        <p>{{ accountNumber }}</p>
+        <p>{{ email }}</p>
+        <!-- <div class="stats">
+          <span>{{ user.following }} Following</span>
+          <span>{{ user.followers }} Followers</span>
+          <span>{{ user.events }} Events</span>
+        </div> -->
+      </div>
+
+      <!-- 用户资料编辑表单 -->
+      <div class="edit-profile">
+        <h4>修改資料</h4>
+
+      </div>
+    </section>
+  </div>
+
 </template>
     
 <script setup>
@@ -12,6 +47,14 @@ import axios from '@/plugins/axios';
 import useUserStore from '@/stores/userstore';
 
 const userStore = useUserStore();
+let accountNumber=ref("");
+let email=ref("");
+let nickname=ref("");
+let phoneNumber=ref("");
+let country=ref("");
+let city=ref("");
+let birthday=ref("");
+let gender=ref("");
 let photo=ref("");
 
 onMounted(function(){
@@ -30,6 +73,10 @@ function callFind() {
         console.log("response1",response1);
         console.log("response2", response2);
 
+        accountNumber.value= response1.data.accountNumber;
+        email.value= response1.data.email;
+        nickname.value = response1.data.nickname;
+
         photo.value = response2.data;
 
     })
@@ -40,6 +87,77 @@ function callFind() {
 }
 </script>
     
-<style>
-    
+<style scoped>
+    .profile-page {
+  display: flex;
+}
+
+.sidebar {
+  width: 200px;
+  background-color: #f0f0f0;
+  padding: 20px;
+}
+
+.sidebar ul {
+  list-style: none;
+  padding: 0;
+}
+
+.sidebar li {
+  margin: 15px 0;
+  cursor: pointer;
+}
+
+.sidebar .active {
+  font-weight: bold;
+  color: #00416a;
+}
+
+.profile-content {
+  flex: 1;
+  padding: 40px;
+  background-color: #fff;
+}
+
+.user-card {
+  background: #f9f9f9;
+  padding: 20px;
+  margin-bottom: 40px;
+  text-align: center;
+}
+
+.user-photo {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-bottom: 20px;
+}
+
+.stats {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+}
+
+.edit-profile {
+  background: #f9f9f9;
+  padding: 20px;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+.form-group label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.save-button {
+  background-color: #00416a;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+}
 </style>
