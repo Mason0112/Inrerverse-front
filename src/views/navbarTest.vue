7a6902af551f40eb820168237de08d24ab4d425e
Nicole <pinhsiuwang@gmail.com>
2024-08-23 11:51:06 +0800

    navbar test good good

--- src/views/navbarTest.vue
+++ src/views/navbarTest.vue
@@ -1,84 +1,140 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <RouterLink class="navbar-brand" :to="{ name: 'home-link' }">InterVerse</RouterLink>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                @click="toggleDropdown" aria-expanded="isDropdownOpen">
-                俱樂部 <font-awesome-icon :icon="['fas', 'caret-down']" :class="{ 'rotate': isDropdownOpen }"/>
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" :class="{ 'show': isDropdownOpen }">
-                <li>
-                  <RouterLink class="dropdown-item" :to="{ name: 'profile-link' }">Edit Profile</RouterLink>
-                </li>
-                <li><a class="dropdown-item" @click="logout">Logout</a></li>
-              </ul>
-            </li>
-            <!-- Other items -->
-          </ul>
-          
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <RouterLink class="nav-link" :to="{ name: 'login-link' }">
-                <font-awesome-icon :icon="['fas', 'sign-in-alt']" /> Login
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" :to="{ name: 'home-link' }">InterVerse</RouterLink>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink class="nav-link active" aria-current="page" :to="{ name: 'home-link' }">首頁</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" :to="{ name: 'login-link' }">登入</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" :to="{ name: 'register-link' }">註冊</RouterLink>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+              @click="toggleDropdown('club')" aria-expanded="dropdownStates.club">
+              dropdown <font-awesome-icon :icon="['fas', 'caret-down']" :class="{ 'rotate': dropdownStates.club }" />
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" :class="{ 'show': dropdownStates.club }">
+              <li><a class="dropdown-item" href="#" @click="closeDropdown('club')">Option 1</a></li>
+              <li><a class="dropdown-item" href="#" @click="closeDropdown('club')">Option 2</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="anotherDropdownMenuLink" role="button"
+              @click="toggleDropdown('settings')" aria-expanded="dropdownStates.settings">
+              設置 <font-awesome-icon :icon="['fas', 'caret-down']" :class="{ 'rotate': dropdownStates.settings }" />
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="anotherDropdownMenuLink"
+              :class="{ 'show': dropdownStates.settings }">
+              <li><a class="dropdown-item" href="#" @click="closeDropdown('settings')">Option 1</a></li>
+              <li><a class="dropdown-item" href="#" @click="closeDropdown('settings')">Option 2</a></li>
+            </ul>
+          </li>
+          <!-- Other items -->
+        </ul>
+
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="anotherDropdownMenuLink" role="button"
+              @click="toggleDropdown('person')" aria-expanded="dropdownStates.person" v-show="userStore.isLoggedIn">
+              我的 <font-awesome-icon :icon="['fas', 'caret-down']" :class="{ 'rotate': dropdownStates.person }" />
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="anotherDropdownMenuLink"
+              :class="{ 'show': dropdownStates.person }">
+              <RouterLink class="dropdown-item" :to="{ name: 'profile-link' }" @click="closeDropdown('person')">編輯個人資料
               </RouterLink>
-            </li>
-            <li class="nav-item">
-              <RouterLink class="nav-link" :to="{ name: 'register-link' }">
-                <font-awesome-icon :icon="['fas', 'user-plus']" /> Register
-              </RouterLink>
-            </li>
-          </ul>
-        </div>
+              <li><a class="dropdown-item" @click="logout(); closeDropdown('person')">登出</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" :to="{ name: 'login-link' }" v-show="!userStore.isLoggedIn">
+              <font-awesome-icon :icon="['fas', 'sign-in-alt']" /> 登入
+            </RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" :to="{ name: 'register-link' }" v-show="!userStore.isLoggedIn">
+              <font-awesome-icon :icon="['fas', 'user-plus']" /> 註冊
+            </RouterLink>
+          </li>
+
+        </ul>
       </div>
-    </nav>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  import useUserStore from '@/stores/userstore';
-  import axios from '@/plugins/axios';
+    </div>
+  </nav>
+</template>
 
-  const isDropdownOpen = ref(false);
+<script setup>
+import { ref } from 'vue';
+import useUserStore from '@/stores/userstore';
+import axios from '@/plugins/axios';
 
-function toggleDropdown() {
-  isDropdownOpen.value = !isDropdownOpen.value;
-}
-  
-  const userStore = useUserStore();
-  
-  function logout() {
-    userStore.resetStore();
-    axios.defaults.headers.authorization ='';
+const dropdownStates = ref({
+  club: false,
+  settings: false,
+  person: false
+});
+
+function initializeDropdownState(menu) {
+  if (!(menu in dropdownStates.value)) {
+    dropdownStates.value[menu] = false;
   }
-  
-  </script>
-  
+}
+
+function toggleDropdown(menu) {
+  initializeDropdownState(menu);
+  Object.keys(dropdownStates.value).forEach(key => {
+    dropdownStates.value[key] = key === menu ? !dropdownStates.value[key] : false;
+  });
+
+}
+
+function closeDropdown(menu) {
+  dropdownStates.value[menu] = false;
+}
+
+const userStore = useUserStore();
+
+function logout() {
+  userStore.resetStore();
+  axios.defaults.headers.authorization = '';
+
+
+}
+
+</script>
+
 
 <style scoped>
 .arrow {
-  font-size: 0.75em; /* Adjust the size of the arrow */
-  transition: transform 0.5s ease; /* Smooth transition for rotation */
+  font-size: 0.75em;
+  /* Adjust the size of the arrow */
+  transition: transform 0.5s ease;
+  /* Smooth transition for rotation */
 }
 
 .rotate {
-  transform: rotate(180deg); /* Rotate the arrow when the dropdown is open */
+  transform: rotate(180deg);
+  /* Rotate the arrow when the dropdown is open */
 }
 
 .dropdown-menu {
-  display: none; /* Hide dropdown menu by default */
+  display: none;
+  /* Hide dropdown menu by default */
   border: none;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 .dropdown-menu.show {
-  display: block; /* Show dropdown menu when it has 'show' class */
+  display: block;
+  /* Show dropdown menu when it has 'show' class */
 }
 
 .dropdown-item {
@@ -91,11 +147,14 @@ function toggleDropdown() {
 }
 
 .dropdown-toggle::after {
-  display: none; /* Hide default Bootstrap dropdown arrow */
+  display: none;
+  /* Hide default Bootstrap dropdown arrow */
 }
 
 .dropdown-toggle {
   display: flex;
   align-items: center;
+  gap: 4px;
+  /* Adjust the gap between the text and the icon */
 }
 </style>
