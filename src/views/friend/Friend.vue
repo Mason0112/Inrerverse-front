e248ebc28ac4268fb85ccafed52ed3021517eec4
nicole <>
2024-08-26 21:57:46 +0800

    friend迭代

--- src/views/friend/Friend.vue
+++ src/views/friend/Friend.vue
@@ -1,5 +1,36 @@
 <template>
-    <h1>我的好友</h1>
+    <h1>我的好友們</h1>
+    <div class="container mt-5">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-10">
+        <div class="card">
+          <div class="input-box">
+
+            <input type="text" class="form-control" />
+            <font-awesome-icon
+              :icon="['fas', 'magnifying-glass']"
+              class="input-icon"
+            />
+          </div>
+
+          <div class="list border-bottom" v-for="friend in friends" :key="user2Id">
+            <!-- <img :src="`${photo}`" alt="User Photo" /> -->
+            <div class="d-flex flex-column ml-3">
+              <span>{{ friend.accountNumber}}</span>
+              <small>{{ friend.age }} 歲</small>
+            </div>
+          </div>
+          <div class="list">
+            <i class="fa fa-weibo"></i>
+            <div class="d-flex flex-column ml-3">
+              <span>Client communication policy</span>
+              <small>#politics - may - @max</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -9,6 +40,11 @@ import useUserStore from "@/stores/userstore";
 
 const userStore = useUserStore();
 let userId = userStore.userId;
+
+const friends = ref([]);
+
+const accountNumber = ref('');
+const age = ref('');
 
 onMounted(function () {
     callFind();
@@ -20,7 +56,13 @@ function callFind() {
         .then(function (response) {
             console.log("response", response);
 
-            fetchUserDetails(response.data);
+            const friendsData = response.data;
+
+      // 将 friend 列表赋值给 friends 变量
+      friends.value = friendsData;
+
+      // 传递 friendsData 去获取每个 user2Id 的详细信息
+      fetchUserDetails(friendsData);
 
         })
         .catch(function (error) {
@@ -28,30 +70,87 @@ function callFind() {
         });
 }
 
-function fetchUserDetails(data) {
-  // Extract user2Ids from the data
-  const user2Ids = data.map(item => item.user2Id);
+function fetchUserDetails(friendsData) {
+  const user2Ids = friendsData.map(friend => friend.user2Id);
 
-  // Create an array of requests to fetch user details for each user2Id
-  const requests = user2Ids.map(user2Id =>
-    axios.get(`/user/secure/${user2Id}`)
-  );
+  const requests = user2Ids.map(user2Id => axios.get(`/user/secure/${user2Id}`));
 
-  // Use Promise.all to handle all requests
   Promise.all(requests)
     .then(responses => {
-      // Process each response
-      responses.forEach(response => {
-        // Log user details
-        console.log('User details:', response.data);
+      // 将用户详情和 friend 关联起来
+      responses.forEach((response, index) => {
+        const userDetails = response.data;
+
+        // 将 userDetails 赋值到对应的 friend 对象中
+        friendsData[index].accountNumber = userDetails.accountNumber;
+        friendsData[index].age = userDetails.age;
       });
+
+      // 更新 friends 变量以触发重新渲染
+      friends.value = friendsData;
     })
     .catch(error => {
-      // Handle any errors that occur during the requests
       console.error('Error fetching user details:', error);
     });
 }
 
 </script>
 
-<style></style>
+<style scoped>
+.input-icon {
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #888;
+  pointer-events: none; /* 防止圖標阻礙用戶點擊輸入框 */
+}
+
+.card {
+  background-color: #fff;
+  padding: 15px;
+  border: none;
+}
+
+.input-box {
+  position: relative;
+}
+
+.input-box i {
+  position: absolute;
+  right: 13px;
+  top: 15px;
+  color: #ced4da;
+}
+
+.form-control {
+  height: 50px;
+  background-color: #eeeeee69;
+}
+
+.form-control:focus {
+  background-color: #eeeeee69;
+  box-shadow: none;
+  border-color: #eee;
+}
+
+.list {
+  padding-top: 20px;
+  padding-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
+.border-bottom {
+  border-bottom: 2px solid #eee;
+}
+
+.list i {
+  font-size: 19px;
+  color: red;
+}
+
+.list small {
+  color: #dedddd;
+}
+</style>
