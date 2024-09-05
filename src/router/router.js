2168d3ee8539f9e227b27ac6e9b6b3610bace388
WIN-10\Administrator <cony7862019@gmail.com>
2024-09-05 13:39:50 +0800

    Revert "工作坊新增成功!"
    This reverts commit cde6824b233d8385342e5504aca46d3ac6c15502.

--- src/router/router.js
+++ src/router/router.js
@@ -42,7 +42,6 @@ import EventList from "@/views/Event/EventList.vue";
 import EventDetail from "@/views/Event/EventDetail.vue";
 import ClubEvent from "@/views/Event/ClubEvent.vue";
 import AddClubEventForm from "@/views/Event/AddClubEventForm.vue";
-import AddWorkshopForm from "@/views/Event/AddWorkshopForm.vue";
 
 
 const routes = [
@@ -76,9 +75,7 @@ const routes = [
     { path: "/event/all",name:"event-all-link", component:EventList},
     { path: "/event/detail/:id",name:"event-detail-link", component:EventDetail},
     { path: "/event/club/all",name:"event-cluball-link", component:ClubEvent},
-    { path: "/event/club/new",name:"event-addclubevent-link", component:AddClubEventForm},
-    { path: "/event/workshop/new",name:"event-addWorkshop-link", component:AddWorkshopForm},
-
+    { path: "/event/club/all",name:"event-addclubevent-link", component:ClubEvent},
 
     //暐欣
     { path: "/product/category", name: "product-category", component: Category},
