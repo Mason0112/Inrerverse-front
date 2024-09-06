584c701aae9b179b4bb8b1a5babb878339e84840
Lance <en7551629@gmail.com>
2024-09-06 15:26:27 +0800

    可以顯示oneArticle

--- src/views/forum/forum.vue
+++ src/views/forum/forum.vue
@@ -1,25 +1,4 @@
 <template>
-    <n-list hoverable clickable>
-      <n-list-item>
-        <n-thing title="相见恨晚" content-style="margin-top: 10px;">
-          <template #description>
-            <n-space size="small" style="margin-top: 4px">
-              <n-tag :bordered="false" type="info" size="small">
-                暑夜
-              </n-tag>
-              <n-tag :bordered="false" type="info" size="small">
-                晚春
-              </n-tag>
-            </n-space>
-          </template>
-            <n-ellipsis style="max-width: 240px">
-                住在我心里孤独的 孤独的海怪 痛苦之王 开始厌倦 深海的光 停滞的海浪
-            </n-ellipsis>
-        </n-thing>
-        
-        
-      </n-list-item>
-    </n-list>
   <div v-for="oneArticle in articleList" :key="oneArticle.id">
       <n-list hoverable clickable>
         <n-list-item @click="enterArticle(oneArticle.id)">
@@ -83,10 +62,12 @@ async function enterArticle(articleId){
   try{
     const response = await axios.get(`/club/article/oneArticle/${articleId}`);
     const articleData = response.data;
+    console.log("Article data from API:",articleData)
 
     router.push({
       name:"club-article-link",
-      params:{ id : articleId},
+      params:{ id : articleId
+      },
       state:{ articleData }
     })
   }catch(error){
